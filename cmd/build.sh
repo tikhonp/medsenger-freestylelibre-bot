bf7d84db615facce940d668992d9da42dff46ec3
TikhonP <tikhon.petrishchev@gmail.com>
2024-04-09 14:51:47 +0300

    modified build script

--- cmd/build.sh
+++ cmd/build.sh
@@ -1,4 +1,4 @@
 export PATH=$PATH:/usr/local/go/bin
 go version
-go build -o /home/medsenger/ctg-medsenger-bot/bin/main cmd/main.go
-go build -o /home/medsenger/ctg-medsenger-bot/bin/fetch_task cmd/fetch_task.go
+go build -o /home/medsenger/medsenger-freestylelibre-bot/bin/main cmd/main.go
+go build -o /home/medsenger/medsenger-freestylelibre-bot/bin/fetch_task cmd/fetch_task.go
